992fb35ad9bc686321053513607ef2ab3d0c1f32
Theo Cole <theo.j.cole@gmail.com>
2021-01-05 16:00:58 +0000

    apply settings correctly

--- yourhonr/settings/base.py
+++ yourhonr/settings/base.py
@@ -15,21 +15,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '2!fndnud=yuax87_sm*1x10u^8o1k)3ez23lbg6^(*%cca!1yn'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
-
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -37,6 +23,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
+    'channels',
     'worklog.apps.WorklogConfig',
 ]
 
@@ -55,8 +43,7 @@ ROOT_URLCONF = 'yourhonr.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'worklog/templates']
-        ,
+        'DIRS': [BASE_DIR / 'worklog/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -70,18 +57,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'yourhonr.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
+ASGI_APPLICATION = 'yourhonr.asgi.application'
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -105,18 +81,21 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
+LANGUAGE_CODE = 'en-gb'
+TIME_ZONE = 'Europe/London'
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = BASE_DIR / 'static'
+STATIC_DIR = STATIC_ROOT
 STATIC_URL = '/static/'
+MEDIA_ROOT = BASE_DIR / 'media' / 'files'
+MEDIA_URL = '/media/'
+
+# Django Extensions
+SHELL_PLUS = 'ipython'
---
+++ yourhonr/settings/example_local.py
@@ -0,0 +1,40 @@
+from yourhonr.settings.base import *
+
+# Security
+DEBUG = True
+ALLOWED_HOSTS = []
+SECRET_KEY = ''
+
+# Database
+DATABASES = {
+    'default': {
+        'ENGINE': '',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+        'TEST': {
+        }
+    }
+}
+
+# Channels
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_rabbitmq.core.RabbitmqChannelLayer',
+        'CONFIG': {
+            'host': 'amqp://<vhost_user>:<vhost_passwd>@127.0.0.1:5672/<vhost>'
+        }
+    }
+}
+
+# Celery
+CELERY_BROKER_URL = 'amqp://<vhost_user>:<vhost_passwd>@127.0.0.1:5672/<vhost>'
+CELERY_RESULT_BACKEND = 'amqp'
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        'LOCATION': '127.0.0.1:11211'
+    }
+}
