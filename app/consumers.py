7c34f0b646fc59e644a78f6f0f47d0c580d6b9e7
Theo Cole <theo.j.cole@gmail.com>
2021-01-08 11:47:14 +0000

    bulk out websocket for easier integration into code

--- app/consumers.py
+++ app/consumers.py
@@ -9,11 +9,19 @@ logger = logging.getLogger('application')
 
 
 class AppConsumer(WebsocketConsumer):
+    """A simple consumer for websocket connections between the client and host.
+
+    Allows for establishing one of multiple potential groups for communication.
+    Messages can be sent to the consumer and then echoed back to the whole group
+    or Python code (eg. Django database signals) can trigger sending of the
+    message host-side using the notify method.
+    """
     def __init__(self):
         super(AppConsumer, self).__init__()
         self.group_name = None
 
     def connect(self):
+        """Join a channel layer group based on the URL used to access socket."""
         # fetch group name from connection request
         self.group_name = self.scope['url_route']['kwargs']['group_name']
         logger.info(f'Received connection request for group: {self.group_name}')
@@ -29,10 +37,38 @@ class AppConsumer(WebsocketConsumer):
         logger.info(f'Connection to {self.group_name} successful!')
 
     def disconnect(self, code):
-        # leave the group
+        """Leave the group with the given code"""
         async_to_sync(self.channel_layer.group_discard)(
             self.group_name,
             self.channel_name
+        )
+
+    def message(self, event):
+        """Send a message
+
+        Combine with channel_layer.group_send to send a message to all in group.
+        """
+        logger.info(f"Sending message: {event['message']}...")
+        self.send(text_data=json.dumps({'message': event['message']}))
+        logger.info(f"Sent!")
+
+    def receive(self, text_data: str=None, bytes_data: bytes=None):
+        """Receive a message from a client within group and broadcast back."""
+        if not text_data and bytes_data:
+            text_data = bytes_data.decode()
+
+        # extract message
+        text_data_json = json.loads(text_data)
+        message = text_data_json['message']
+        logger.info(f'received message: {message}')
+
+        # send message back
+        async_to_sync(self.channel_layer.group_send)(
+            self.group_name,
+            {
+                'type': 'message',
+                'message': message
+            }
         )
 
     def notify(self, event):
@@ -51,27 +87,3 @@ class AppConsumer(WebsocketConsumer):
                 'message': event['content']
             }
         )
-
-    def receive(self, text_data: str=None, bytes_data: bytes=None):
-        """Receive a message from a client and echo message back."""
-        if not text_data and bytes_data:
-            text_data = bytes_data.decode()
-
-        # extract message
-        text_data_json = json.loads(text_data)
-        message = text_data_json['message']
-        logger.info(f'received message: {message}')
-
-        # send message back
-        async_to_sync(self.channel_layer.group_send)(
-            self.group_name,
-            {
-                'type': 'message',
-                'message': message
-            }
-        )
-
-    def message(self, event):
-        logger.info(f"Sending message: {event['message']}...")
-        self.send(text_data=json.dumps({'message': event['message']}))
-        logger.info(f"Sent!")
