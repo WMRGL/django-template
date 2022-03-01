1f102281216882d7edd9f6e5bef4a12e624e1bb4
Theo Cole <theo.cole@nhs.net>
2022-03-01 12:51:13 +0000

    load ASGI app early to ensure models loaded

--- project/asgi.py
+++ project/asgi.py
@@ -8,16 +8,18 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 """
 import os
 
+from django.core.asgi import get_asgi_application
+django_asgi_app = get_asgi_application()
+
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
 
 import app.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings.local')
 
 application = ProtocolTypeRouter({
-    'http': get_asgi_application(),
+    'http': django_asgi_app,
     'websocket': AuthMiddlewareStack(
         URLRouter(
             app.routing.websocket_urlpatterns
